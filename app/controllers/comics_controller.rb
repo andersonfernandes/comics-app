7fff3114505525482787c6b40d162a753a5ca129
Anderson Fernandes <fernandesanderson14@gmail.com>
2021-07-03 22:50:41 -0300

    Configured React App startup
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

---
+++ app/controllers/comics_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class ComicsController < ApplicationController
+  def index; end
+end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,11 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+import React from 'react'
+import { render } from 'react-dom'
 
-import Rails from "@rails/ujs"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import App from './components/App'
 
-Rails.start()
-ActiveStorage.start()
+document.addEventListener("DOMContentLoaded", () => {
+  render(
+    <App />,
+    document.getElementById('root')
+  )
+})
