1a28bd78a2dcdcc162541ba9265d58959896503a
Anderson Fernandes <fernandesanderson14@gmail.com>
2021-07-04 10:52:38 -0300

    Setup jest test suit
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

---
+++ app/javascript/packs/components/App/App.test.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { render, unmountComponentAtNode } from "react-dom"
+import { act } from "react-dom/test-utils"
+
+import App from "./App"
+
+let container = null;
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders the app greeting", () => {
+  act(() => {
+    render(<App />, container);
+  });
+  expect(container.textContent).toBe("Comics App");
+});
---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverage: false,
+  coverageDirectory: "coverage",
+  roots: [
+    "app/javascript/packs"
+  ],
+  testEnvironment: "jsdom",
+};
